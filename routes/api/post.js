4a84df3d7af76aaccfedd2fc2c0dab994653987d
mohit <mohitpardeshi729@gmail.com>
2023-02-26 11:59:47 +0530

    code cleaned

--- routes/api/post.js
+++ routes/api/post.js
@@ -6,12 +6,13 @@ const User = require('../../models/User');
 const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 
-// @route       POST api/post
-// @description CREATE a post
-// @access      Public
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
 router.post(
   '/',
-  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  auth,
+  check('text', 'Text is required').notEmpty(),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -19,18 +20,21 @@ router.post(
     }
 
     try {
-      const user = await User.findById(req.user.id).select('password');
+      const user = await User.findById(req.user.id).select('-password');
+
       const newPost = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
-        user: req.user.id,
+        user: req.user.id
       });
+
       const post = await newPost.save();
+
       res.json(post);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send('server error');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
     }
   }
 );
