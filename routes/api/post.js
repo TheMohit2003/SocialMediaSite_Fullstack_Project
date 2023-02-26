11165c0efbf3c9cb0ff482b212fe983aebfb3fb2
mohit <mohitpardeshi729@gmail.com>
2023-02-26 12:34:42 +0530

    added route to like post

--- routes/api/post.js
+++ routes/api/post.js
@@ -73,4 +73,53 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
+// @route    DELETE api/posts/:id
+// @desc     DELETE post by id
+// @access   Private
+
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    } else {
+      await post.remove();
+      return res.status(200).json({ msg: 'Post removed' });
+    }
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    PUT api/posts/:id
+// @desc     Like a post
+// @access   Private
+
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //check if the user has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.json(400).json({ msg: 'post already liked' });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error');
+  }
+});
+
 module.exports = router;
