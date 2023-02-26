9e67030e55c8a9df89fad3686591d10970417773
mohit <mohitpardeshi729@gmail.com>
2023-02-26 12:39:10 +0530

    dislike route added

--- routes/api/post.js
+++ routes/api/post.js
@@ -122,4 +122,33 @@ router.put('/like/:id', auth, async (req, res) => {
   }
 });
 
+// @route    PUT api/posts/:id
+// @desc     Like a post
+// @access   Private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //check if the user has already been unliked
+    if (
+      (post.likes.filter(
+        (like) => like.user.toString() === req.user.id
+      ).length = 0)
+    ) {
+      return res.status(400).json({ msg: 'post has not been liked' });
+    }
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error');
+  }
+});
+
 module.exports = router;
