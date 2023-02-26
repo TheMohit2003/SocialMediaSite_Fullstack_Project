b29b807211d3573e2df95777bd71b71ce6e36d4c
mohit <mohitpardeshi729@gmail.com>
2023-02-26 12:18:36 +0530

    added get route and specific id get post route

--- routes/api/post.js
+++ routes/api/post.js
@@ -26,7 +26,7 @@ router.post(
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
-        user: req.user.id
+        user: req.user.id,
       });
 
       const post = await newPost.save();
@@ -38,5 +38,39 @@ router.post(
     }
   }
 );
+
+// @route    GET api/posts
+// @desc     GET all posts
+// @access   Private
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    GET api/posts/:id
+// @desc     GET post by id
+// @access   Private
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'post not found' });
+    }
+    res.json(post);
+  } catch (error) {
+    console.error(err.message);
+    if (!err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
